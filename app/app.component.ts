55a07f42369f7fae315c70d3c5142462165df42d
Ryan Transfiguracion <rtransfig@gmail.com>
2016-08-09 16:04:20 -0700

    - Part 5 done. - Added hero service class that retrives the mock hero data and provides it to the app component. - Implemented Promise to asynchronously provide the mock hero data.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -64,30 +66,39 @@ import { HeroDetailComponent } from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
-export class AppComponent 
+export class AppComponent implements OnInit
 {
+  ngOnInit()
+  {
+    this.getHeroes();
+  }
+
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  public heroes = HEROES;
+  heroes: Hero[];
 
   onSelect(hero: Hero)
   {
     this.selectedHero = hero;
   }
+
+  constructor(private heroService: HeroService)
+  {
+
+  }
+
+  getHeroes()
+  {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+
+    // *** NON-ARROW-FUNCTION EQUIVALENT ***
+    // let self = this;
+    // this.heroService.getHeroes().then(function(heroes)
+    // {
+    //   self.heroes = heroes;
+    // });
+  }
 }
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
