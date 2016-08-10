d22836777fcf85093b2e6ff555bbc080e319a864
Ryan Transfiguracion <rtransfig@gmail.com>
2016-08-10 13:10:17 -0700

    - RC5 Part 5 finished. - No difference from RC4 Part 5 Services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 // import { HeroDetailComponent } from './hero-detail.component';  // an RC4 implementation, now done in NgModule decorator
 
@@ -64,30 +66,32 @@ import { Hero } from './hero';
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
-export class AppComponent 
-{
+export class AppComponent implements OnInit
+{  
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  public heroes = HEROES;
+  heroes: Hero[];
 
   onSelect(hero: Hero)
   {
     this.selectedHero = hero;
   }
+
+  constructor(private heroService: HeroService)
+  {
+
+  }
+
+  getHeroes()
+  {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit()
+  {
+    this.getHeroes();
+  }
 }
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
